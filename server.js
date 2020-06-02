b496bb4097f9714a7cdfa926bd318e71ec1643bc
Chris Little <chrisdlittle@aol.com>
2020-06-02 13:34:36 -0400

    init commit/started server

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
