98192e320538b1acefde7794c0a14573f3a6b64e
Chris Little <chrisdlittle@aol.com>
2020-06-03 16:19:24 -0400

    got html files to serve up with routes/set up model for users/configured server for users

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 const express = require("express");
+const session = require("express-session");
+const passport = require("./config/passport");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+var db = require("./models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
+/* 
 const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.set("view engine", "handlebars"); */
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-const routes = require("./controllers/user_controller.js");
+require("./controllers/html-routes.js")(app);
+require("./controllers/api-routes.js")(app);
 
-app.use(routes);
-
-app.listen(PORT, function () {
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+      console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    });
+  });
