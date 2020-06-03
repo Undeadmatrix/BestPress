332030ce1c5f4268d8d71c0104b838fbbe0b0491
Chris Little <chrisdlittle@aol.com>
2020-06-03 13:49:06 -0400

    got server to render a page

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -4,3 +4,5 @@ const router = express.Router();
 router.get("/", function (req, res) {
     res.send("page rendered");
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//const routes = require("#");
+const routes = require("./controllers/user_controller.js");
 
-//app.use(routes);
+app.use(routes);
 
 app.listen(PORT, function () {
   console.log("Server listening on: http://localhost:" + PORT);
