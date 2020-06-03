f7ac71bfe42edb879db303f635e7b77e92733b5e
Chris Little <chrisdlittle@aol.com>
2020-06-03 11:27:28 -0400

    added .env and worked on server.js

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-var express = require("express");
+const express = require("express");
 
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("#");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
