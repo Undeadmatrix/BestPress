793776dd7010f3dd26dd42b94928550c2d00d706
Chris Little <chrisdlittle@aol.com>
2020-06-04 14:10:49 -0400

    added route template to connect to createpost/

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -28,4 +28,8 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 
+  app.get("/createpost", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
+  });
+
 };
