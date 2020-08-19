1b1a7a1b6b35f6bb849b22bd995bd6a364788478
Chris Little <61989359+Undeadmatrix@users.noreply.github.com>
2020-08-19 15:16:52 -0400

    Revert "added account tab to view your own posts"

--- controllers/post-api-routes.js
+++ controllers/post-api-routes.js
@@ -7,50 +7,11 @@ module.exports = function (app) {
     if (req.query.author_id) {
       query.AuthorId = req.query.author_id;
     }
-
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Author
     db.Post.findAll({
       where: query,
-      include: [db.Author]
-    }).then(function (dbPost) {
-      //console.log(dbPost);
-      //console.log(dbPost[0].dataValues.id);
-      //console.log(dbPost[0].dataValues.title);
-      //console.log(dbPost[0].dataValues.body);
-      const data = [];
-      console.log("-------LENGTH--------");
-      console.log(dbPost.length);
-      console.log("-------LENGTH--------");
-      for(var i = 0; i < dbPost.length; i++)
-      {
-        data.push(dbPost[i].dataValues);
-      }
-      console.log("-------ARRAY DATA--------");
-      //console.log(data);
-      console.log("-------ARRAY DATA--------");
-      //res.render("handlebar name file", { dbPost } ) sends to hb file
-      res.send(data);
-    });
-  });
-
-  // GET route for getting all of the posts by whoever is signed in
-  app.get("/api/myposts/:id", function (req, res) {
-    var query = {};
-    if (req.query.author_id) {
-      query.AuthorId = req.query.author_id;
-    }
-    console.log("THIS IS A TEST");
-    console.log("route param id: ",req.params.id);
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Author
-    db.Post.findAll({
-      where: {
-        //author id = user logged in
-        AuthorId: req.params.id
-      },
       include: [db.Author]
     }).then(function (dbPost) {
       console.log(dbPost);
